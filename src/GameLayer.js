aceb4e55e6ca683ff7095f3551c189f6e676cdf5
Kundjanasith <kundjanasith@gmail.com>
2015-04-03 11:20:50 +0700

    edit press ship

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -3,10 +3,37 @@ var GameLayer = cc.LayerColor.extend({
         this._super( new cc.Color( 0, 0, 0, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
 
+        this.initComponent();
+
+        this.addKeyboardHandlers();
+        this.scheduleUpdate();
+        
+        return true;
+    },
+
+    initComponent:function(){
+        this.createShip();
+        this.createBullet();
+        this.createLifeLabel();
+
+    },
+
+    createShip:function(){
         this.ship = new Ship();
         this.ship.setPosition( new cc.Point( 700, 300 ) );
         this.addChild( this.ship );
 
+    },
+
+    createLifeLabel:function(){
+        this.life = 10;
+        this.lifeLabel = cc.LabelTTF.create( 10, 'Arial', 32 );
+        this.lifeLabel.setPosition( cc.p( 700, 550 ) );
+        this.addChild( this.lifeLabel );
+
+    },
+
+    createBullet:function(){
         this.bullets = [];
         for ( var i = 0; i < 15; i++ ) {
             var p = Math.random();
@@ -25,16 +52,6 @@ var GameLayer = cc.LayerColor.extend({
 
             this.bullets.push( bullet );
         }
-
-        this.life = 10;
-        this.lifeLabel = cc.LabelTTF.create( this.life , 'Arial', 32 );
-        this.lifeLabel.setPosition( cc.p( 700, 550 ) );
-        this.addChild( this.lifeLabel );
-        
-        this.addKeyboardHandlers();
-        this.scheduleUpdate();
-        
-        return true;
     },
 
     update: function( dt ) {
@@ -42,41 +59,29 @@ var GameLayer = cc.LayerColor.extend({
         this.bullets.forEach( function( bullet, i ) {
             var x = bullet.getPositionX();
             if ( ( x < screenWidth ) &&
-                 ( x > screenWidth - 100 ) ) {
+               ( x > screenWidth - 100 ) ) {
                 var y = bullet.getPositionY();
-                if ( Math.abs( y - self.ship.getPositionY() ) < 25 ) {
-                    bullet.randomPosition();
-                    self.life -= 1;
-                    self.lifeLabel.setString( self.life );
-                    if ( self.life == 0 ) {
-                        var gameOverLabel = cc.LabelTTF.create( 'Game over', 'Arial', 60 );
-                        gameOverLabel.setPosition( cc.p( 400, 300 ) );
-                        self.addChild( gameOverLabel );
-                        cc.director.pause();
-                    }
-                    return;
-                }
-            }
-            if ( x > screenWidth ) {
+            if ( Math.abs( y - self.ship.getPositionY() ) < 25 ) {
                 bullet.randomPosition();
+                self.life -= 1;
+                self.lifeLabel.setString( self.life );
+                if ( self.life == 0 ) {
+                    var gameOverLabel = cc.LabelTTF.create( 'Game over', 'Arial', 60 );
+                    gameOverLabel.setPosition( cc.p( 400, 300 ) );
+                    self.addChild( gameOverLabel );
+                    cc.director.pause();
+                }
+                return;
             }
-        });
+        }
+        if ( x > screenWidth ) {
+            bullet.randomPosition();
+        }
+    });
     },
     
     onKeyDown: function( keyCode, event ) {
-        if ( keyCode == cc.KEY.up ) {
-            var y = this.ship.getPositionY();
-            if ( y < screenHeight - 10 ) {
-                y += 10;
-                this.ship.setPositionY( y );
-            }
-        } else if ( keyCode == cc.KEY.down ) {
-            var y = this.ship.getPositionY();
-            if ( y > 10 ) {
-                y -= 10;
-                this.ship.setPositionY( y );
-            }
-        }
+        this.ship.press(keyCode);
     },
     
     onKeyUp: function( keyCode, event ) {
@@ -95,7 +100,7 @@ var GameLayer = cc.LayerColor.extend({
         }, this);
     }
 });
- 
+
 var StartScene = cc.Scene.extend({
     onEnter: function() {
         this._super();
--- src/Ship.js
+++ src/Ship.js
@@ -1,6 +1,27 @@
 var Ship = cc.Sprite.extend({
+
     ctor: function() {
         this._super();
         this.initWithFile( 'res/images/ship.png' );
+    }, 
+
+    press: function(press){
+    	if(press==cc.KEY.up){
+    		var y = this.getPositionY();
+            if ( y < screenHeight - 10 ) {
+                y += 10;
+                this.setPositionY( y );
+            }
+    	}
+    	if(press==cc.KEY.down){
+    		  var y = this.getPositionY();
+            if ( y > 10 ) {
+                y -= 10;
+                this.setPositionY( y );
+            }
+
+
+    	}
     }
+
 });
