729bbe535305ff83f283a41a46b7ac270dbc7488
Kundjanasith <kundjanasith@gmail.com>
2015-04-03 11:21:18 +0700

    add src/ship.js

--- src/Ship.js
+++ src/Ship.js
@@ -1,27 +1,26 @@
 var Ship = cc.Sprite.extend({
 
-    ctor: function() {
-        this._super();
-        this.initWithFile( 'res/images/ship.png' );
-    }, 
+	ctor: function() {
+		this._super();
+		this.initWithFile( 'res/images/ship.png' );
+	}, 
 
-    press: function(press){
-    	if(press==cc.KEY.up){
-    		var y = this.getPositionY();
-            if ( y < screenHeight - 10 ) {
-                y += 10;
-                this.setPositionY( y );
-            }
-    	}
-    	if(press==cc.KEY.down){
-    		  var y = this.getPositionY();
-            if ( y > 10 ) {
-                y -= 10;
-                this.setPositionY( y );
-            }
+	press: function(press){
+		if(press==cc.KEY.up){
+			var y = this.getPositionY();
+			if ( y < screenHeight - 10 ) {
+				y += 10;
+				this.setPositionY( y );
+			}
+		}
+		if(press==cc.KEY.down){
+			var y = this.getPositionY();
+			if ( y > 10 ) {
+				y -= 10;
+				this.setPositionY( y );
+			}
 
-
-    	}
-    }
+		}
+	}
 
 });
